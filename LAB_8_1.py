d2fdfef1f5084b865ec347d6acd83d20ed9c8bcf
shantitudu <48344175+shantitudu@users.noreply.github.com>
2024-03-18 21:22:20 +0530

    Update LAB_8_1.py

--- LAB_8_1.py
+++ LAB_8_1.py
@@ -1,38 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #Given input as salary of three months (jan,feb,march),write a program that returns the total salary for quarter 1(Q1)
 import mysql.connector
-
-
-# In[3]:
-
-
-import os
-
-
-# In[4]:
-
-
 cnx=mysql.connector.connect(
     host="localhost",
     user= "root",
     password="",
     database="lab1"
 )
-
-
-# In[5]:
-
-
 cur=cnx.cursor()
-
-
-# In[ ]:
-
 
 cur.execute("""
             CREATE FUNCTION  calculate_Q_salary (jan_sal FLOAT,feb_sal FLOAT,march_sal FLOAT)
@@ -44,21 +18,8 @@ cur.execute("""
             END;
         """)
 
-
-# In[ ]:
-
-
 cur.execute("""SELECT calculate_Q_salary(1500.00,2000.00,1000.00) AS Q_salary;""")
 
-
-# In[ ]:
-
-
 cur.close()
-
-
-# In[ ]:
-
-
 cnx.close()
 
